623761e15d810eb07e90820d8cbc42e41fe8b968
grtld <deagretal@gmail.com>
2018-01-13 03:08:49 -0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -13,13 +13,14 @@ public class Server {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         
-        String testURL = "/test?v=abc";
+        String testURL = "/test?s=1&c=on";
         URIBuilder builder = new URIBuilder(testURL);
         List<NameValuePair> params = builder.getQueryParams();
-        String var = params.get(0).getValue();
+        String switch = params.get(0).getValue();
+        String command = params.get(1).getValue();
         
         //basically each url location launches new methods i think
-        server.createContext(test, new MyHandler());
+        server.createContext(test, new MyHandler(switch, command));
         server.createContext("/test2", new MyHandler2());
         
         server.setExecutor(null); // creates a default executor
@@ -27,9 +28,17 @@ public class Server {
     }
 
     static class MyHandler implements HttpHandler {
+        String switch;
+        String command;
+        
+        public MyHandler(String s, String c) {
+            switch = s;
+            command = c
+        }
+        
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response";
+            String response = "Switch: " + switch + ", Command: " + command;
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
