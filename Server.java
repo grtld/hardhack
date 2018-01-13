eed7cd7929275d1102b3ab5cc29a4eaad43ccd75
grtld <deagretal@gmail.com>
2018-01-13 02:54:11 -0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -6,13 +6,20 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import javax.ws.rs.core.UriBuilder;
+
 public class Server {
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         
+        String testURL = "/test?v=abc";
+        URIBuilder builder = new URIBuilder(testURL);
+        List<NameValuePair> params = builder.getQueryParams();
+        String var = params.get(0).getValue();
+        
         //basically each url location launches new methods i think
-        server.createContext("/test", new MyHandler());
+        server.createContext(test, new MyHandler());
         server.createContext("/test2", new MyHandler2());
         
         server.setExecutor(null); // creates a default executor
