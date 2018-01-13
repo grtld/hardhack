445e3a818385063b318ccb3aef21d463f5aad9a4
Gretal Dea <deagretal@gmail.com>
2018-01-13 11:37:28 -0800

    added toast and buttons for another switch

--- lightapp/app/src/main/java/com/example/codegirl/lightapp/MainActivity.java
+++ lightapp/app/src/main/java/com/example/codegirl/lightapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.codegirl.lightapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView myTextView;
@@ -19,5 +21,20 @@ public class MainActivity extends AppCompatActivity {
         String tag = view.getTag().toString();
         String[] tag_parts = tag.split("_");
         myTextView.setText("Switch: " + tag_parts[1] + "\nOn/Off: " + tag_parts[2]);
+
+        Context context = getApplicationContext();
+        String status;
+        String switchTag = tag_parts[1];
+        if(tag_parts[2].equals("0")) {
+            status = "OFF";
+        }
+        else {
+            status = "ON";
+        }
+        CharSequence text = "Switch " + switchTag + " " + status;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
